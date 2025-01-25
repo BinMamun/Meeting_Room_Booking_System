4bd57f661b1f3bf9dcecc64b3e7505e2bfaeae91
Abdullah Bin Mamun <binbean44@gmail.com>
2025-01-25 23:41:01 +0600

    Create initial project

---
+++ src/Meeting_Room_Booking_System/Meeting_Room_Booking_System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Meeting_Room_Booking_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
