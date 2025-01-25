efe7f557b1500e04a1bfc247578d59a6f96749a5
Abdullah Bin Mamun <binbean44@gmail.com>
2025-01-26 00:39:17 +0600

    add constructor to get connectionstring and migrationAssembly in ApplicationDbContext

--- src/Meeting_Room_Booking_System/MeetingRoomBookingSystem.Infrastructure/ApplicationDbContext.cs
+++ src/Meeting_Room_Booking_System/MeetingRoomBookingSystem.Infrastructure/ApplicationDbContext.cs
@@ -5,9 +5,13 @@ namespace MeetingRoomBookingSystem.Infrastructure
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        private readonly string _connectionString;
+        private readonly string _migrationAssembly;
+
+        public ApplicationDbContext(string connectionString, string migrationAssembly)
         {
+            _connectionString = connectionString;
+            _migrationAssembly = migrationAssembly;
         }
     }
 }
