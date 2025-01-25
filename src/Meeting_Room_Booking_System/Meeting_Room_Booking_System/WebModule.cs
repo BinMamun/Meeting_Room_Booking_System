fd2844bc5f4fb05a57bf7b83f9ae7cffae9e306c
Abdullah Bin Mamun <binbean44@gmail.com>
2025-01-26 00:26:08 +0600

    Create WebModule class and inherit from Module class of Autofac

---
+++ src/Meeting_Room_Booking_System/Meeting_Room_Booking_System/WebModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace MeetingRoomBookingSystem.Web
+{
+    public class WebModule(string connectionString, string migrationAssembly) : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
+    }
+}
