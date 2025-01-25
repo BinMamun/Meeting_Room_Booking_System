09b030f9c9106ec4405c5b65e96a654805e3bcd6
Abdullah Bin Mamun <binbean44@gmail.com>
2025-01-26 00:56:47 +0600

    Create ApplicationUser class and inherit from IdentityUser

---
+++ src/Meeting_Room_Booking_System/MeetingRoomBookingSystem.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MeetingRoomBookingSystem.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int Pin {  get; set; }
+        public string Department {  get; set; }
+    }
+}
