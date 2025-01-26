cfed60746fa52a2d9e2db155b0eabdecadbd992a
Abdullah Bin Mamun <binbean44@gmail.com>
2025-01-26 20:49:27 +0600

    Create WebModule class to register DI

---
+++ src/MeetingRoomBookingSystem/MeetingRoomBookingSystem.Web/WebModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using MeetingRoomBookingSystem.Infrastructure;
+
+namespace MeetingRoomBookingSystem.Web
+{
+    public class WebModule(string connectionString, string migrationAssembly) : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ApplicationDbContext>().AsSelf()
+                .WithParameter("connectionString", connectionString)
+                .WithParameter("migrationAssembly", migrationAssembly)
+                .InstancePerLifetimeScope();
+
+            base.Load(builder);
+        }
+    }
+}
