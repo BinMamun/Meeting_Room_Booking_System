2008c82a23c48d0366b648edb58ce920f04607bb
Abdullah Bin Mamun <binbean44@gmail.com>
2025-01-27 01:13:58 +0600

    Create ResponseModel.cs

---
+++ src/MeetingRoomBookingSystem/MeetingRoomBookingSystem.Web/Areas/Admin/Models/ResponseModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MeetingRoomBookingSystem.Web.Areas.Admin.Models
+{
+    public enum ResponseTypes
+    {
+        Success,
+        Danger,
+        Warning
+    }
+    public class ResponseModel
+    {
+        public string? Message { get; set; }
+        public ResponseTypes Type { get; set; }
+    }
+}
