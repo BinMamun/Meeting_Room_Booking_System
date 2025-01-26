ece53f52954092b7c059e15e5b5a36d6092369f8
Abdullah Bin Mamun <binbean44@gmail.com>
2025-01-26 22:13:08 +0600

    Create Repository class and implement IRepositoryBase nad IRepository interface

---
+++ src/MeetingRoomBookingSystem/MeetingRoomBookingSystem.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,98 @@
+﻿using System.Linq.Expressions;
+using MeetingRoomBookingSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace MeetingRoomBookingSystem.Infrastructure.Repositories
+{
+    public abstract class Repository<TEntity, TKey>
+        : IRepository<TEntity, TKey> where TKey : IComparable
+        where TEntity : class, IEntity<TKey>
+    {
+        private DbContext _dbContext;
+        private DbSet<TEntity> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _dbContext = context;
+            _dbSet = _dbContext.Set<TEntity>();
+        }
+
+        public virtual async Task AddAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public virtual async Task<TEntity> GetByIdAsync(TKey id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public virtual async Task<IList<TEntity>> GetAllAsync()
+        {
+            IQueryable<TEntity> query = _dbSet;
+            return await query.ToListAsync();
+        }
+
+        public virtual async Task EditAsync(TEntity entityToUpdate)
+        {
+            await Task.Run(() =>
+            {
+                _dbSet.Attach(entityToUpdate);
+                _dbContext.Entry(entityToUpdate).State = EntityState.Modified;
+            });
+        }
+
+        public virtual async Task RemoveAsync(TKey id)
+        {
+            var entityToDelete = _dbSet.Find(id);
+            await RemoveAsync(entityToDelete);
+        }
+
+        public virtual async Task RemoveAsync(TEntity entityToDelete)
+        {
+            await Task.Run(() =>
+            {
+                if (_dbContext.Entry(entityToDelete).State == EntityState.Detached)
+                {
+                    _dbSet.Attach(entityToDelete);
+                }
+                _dbSet.Remove(entityToDelete);
+            });
+        }
+
+        public virtual async Task<IList<TEntity>> GetDynamicAsync(
+            Expression<Func<TEntity, bool>> filter = null,
+            string orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+            bool isTrackingOff = false)
+        {
+            IQueryable<TEntity> query = _dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (include != null)
+                query = include(query);
+
+            if (orderBy != null)
+            {
+                var result = query.OrderBy(orderBy);
+
+                if (isTrackingOff)
+                    return await result.AsNoTracking().ToListAsync();
+                else
+                    return await result.ToListAsync();
+            }
+            else
+            {
+                if (isTrackingOff)
+                    return await query.AsNoTracking().ToListAsync();
+                else
+                    return await query.ToListAsync();
+            }
+        }
+    }
+}
