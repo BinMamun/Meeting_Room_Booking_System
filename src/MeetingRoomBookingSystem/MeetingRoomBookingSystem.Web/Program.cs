276fee5aa9247680b5fe323749e29f96895b3005
Abdullah Bin Mamun <binbean44@gmail.com>
2025-01-26 21:09:16 +0600

    Add a new area and add new area view folders

--- src/MeetingRoomBookingSystem/MeetingRoomBookingSystem.Web/Program.cs
+++ src/MeetingRoomBookingSystem/MeetingRoomBookingSystem.Web/Program.cs
@@ -82,6 +82,11 @@ try
     app.MapStaticAssets();
 
     app.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}")
+        .WithStaticAssets();
+
+    app.MapControllerRoute(
         name: "default",
         pattern: "{controller=Home}/{action=Index}/{id?}")
         .WithStaticAssets();
