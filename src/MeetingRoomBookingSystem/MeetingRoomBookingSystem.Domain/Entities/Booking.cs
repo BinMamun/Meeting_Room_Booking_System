b4bf018b141c37901c6d87300a8b31c354d29818
Abdullah Bin Mamun <binbean44@gmail.com>
2025-01-26 23:55:38 +0600

    Add navlinks in layout

--- src/MeetingRoomBookingSystem/MeetingRoomBookingSystem.Domain/Entities/Booking.cs
+++ src/MeetingRoomBookingSystem/MeetingRoomBookingSystem.Domain/Entities/Booking.cs
@@ -7,6 +7,8 @@ namespace MeetingRoomBookingSystem.Domain.Entities
         public string MeetingTitle { get; set; }
         public DateTime Meetingtime { get; set; }
         public string? MeetingPurpose { get; set; }
+        //public TimeSpan StartTime { get; set; }
+        //public TimeSpan? EndTime { get; set; }
         public Guid MeetingRoomId { get; set; }
         public MeetingRoom MeetingRoom { get; set; }
         
