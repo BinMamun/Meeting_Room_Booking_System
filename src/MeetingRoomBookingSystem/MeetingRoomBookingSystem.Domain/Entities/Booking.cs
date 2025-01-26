959153225a91012e664790d3e5d8038543591b52
Abdullah Bin Mamun <binbean44@gmail.com>
2025-01-26 23:09:11 +0600

    Add MeetingRoom and Booking Entity

---
+++ src/MeetingRoomBookingSystem/MeetingRoomBookingSystem.Domain/Entities/Booking.cs
@@ -0,0 +1,14 @@
+﻿
+namespace MeetingRoomBookingSystem.Domain.Entities
+{
+    public class Booking : IEntity<Guid>
+    {
+        public Guid Id { get; set; }
+        public string MeetingTitle { get; set; }
+        public DateTime Meetingtime { get; set; }
+        public string? MeetingPurpose { get; set; }
+        public Guid MeetingId { get; set; }
+        public MeetingRoom MeetingRoom { get; set; }
+        
+    }
+}
---
+++ src/MeetingRoomBookingSystem/MeetingRoomBookingSystem.Domain/Entities/MeetingRoom.cs
@@ -0,0 +1,18 @@
+﻿
+namespace MeetingRoomBookingSystem.Domain.Entities
+{
+    public class MeetingRoom : IEntity<Guid>
+    {
+        public Guid Id { get; set; }
+        public string MeetingRoomTitle { get; set; }
+        public string? Description { get; set; }
+        public string? Color { get; set; }
+        public string? Image { get; set; }
+        public string? Location { get; set; }
+        public int Capacity { get; set; }
+        public string? Facilities { get; set; }
+        public bool IsActive { get; set; }
+        public DateOnly? Available { get; set; }
+
+    }
+}
