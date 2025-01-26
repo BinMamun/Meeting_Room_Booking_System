553ab5772b04d9e67aa5d64cfd6b0e1e982803e9
Abdullah Bin Mamun <binbean44@gmail.com>
2025-01-26 12:59:34 +0600

    Create LogInModel for user log in

---
+++ src/Meeting_Room_Booking_System/MeetingRoomBookingSystem.Web/Models/LogInModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeetingRoomBookingSystem.Web.Models
+{
+    public class LogInModel
+    {
+        [Required]
+        public int Pin { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+
+        public string? ReturnUrl { get; set; }
+        public IList<AuthenticationScheme>? ExternalLogins { get; set; }
+
+    }
+}
