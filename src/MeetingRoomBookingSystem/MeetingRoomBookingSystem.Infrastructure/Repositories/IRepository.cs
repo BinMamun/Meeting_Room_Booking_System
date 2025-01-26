a934c4f046e2666cc79d77cdc3dcd8a24b7b88ab
Abdullah Bin Mamun <binbean44@gmail.com>
2025-01-26 21:59:20 +0600

    Add IRepository interface in infrastructure

---
+++ src/MeetingRoomBookingSystem/MeetingRoomBookingSystem.Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using MeetingRoomBookingSystem.Domain.Entities;
+using MeetingRoomBookingSystem.Domain.RepositoryContracts;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace MeetingRoomBookingSystem.Infrastructure.Repositories
+{
+    public interface IRepository<TEntity, TKey> : IRepositoryBase<TEntity, TKey>
+        where TEntity : class, IEntity<TKey>
+        where TKey : IComparable
+    {
+        Task<IList<TEntity>> GetDynamicAsync(Expression<Func<TEntity, bool>> filter = null, string orderBy = null, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null, bool isTrackingOff = false);
+    }
+}
