dc18bf2a3faad36404d495201d7fa8f87aa193c9
Abdullah Bin Mamun <binbean44@gmail.com>
2025-01-27 00:32:51 +0600

    Create MeetingRoomController.cs

---
+++ src/MeetingRoomBookingSystem/MeetingRoomBookingSystem.Web/Areas/Admin/Controllers/MeetingRoomController.cs
@@ -0,0 +1,20 @@
+﻿using MeetingRoomBookingSystem.Domain.ServiceContracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingRoomBookingSystem.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MeetingRoomController(
+        ILogger<MeetingRoomController> logger,
+        IMeetingRoomManagementService meetingRoomService
+        ) : Controller
+
+    {
+        private readonly ILogger<MeetingRoomController> _logger = logger;
+        private readonly IMeetingRoomManagementService _meetingRoomService = meetingRoomService;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
