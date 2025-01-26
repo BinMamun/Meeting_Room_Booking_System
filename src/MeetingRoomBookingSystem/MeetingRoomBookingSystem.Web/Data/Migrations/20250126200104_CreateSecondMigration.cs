bd9ddd883dd9774bb7616b07f10ad13cf1a4ce0f
Abdullah Bin Mamun <binbean44@gmail.com>
2025-01-27 02:08:38 +0600

    Generate new migration

---
+++ src/MeetingRoomBookingSystem/MeetingRoomBookingSystem.Web/Data/Migrations/20250126200104_CreateSecondMigration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeetingRoomBookingSystem.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateSecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Available",
+                table: "MeetingRooms");
+
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "MeetingRooms");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Availablity",
+                table: "MeetingRooms",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Availablity",
+                table: "MeetingRooms");
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "Available",
+                table: "MeetingRooms",
+                type: "date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "MeetingRooms",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
