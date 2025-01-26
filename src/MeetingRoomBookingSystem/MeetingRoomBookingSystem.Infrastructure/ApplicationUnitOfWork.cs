8366a9ae51949e9f35b7cbc02497e1acd0c068bc
Abdullah Bin Mamun <binbean44@gmail.com>
2025-01-26 23:08:18 +0600

    Add UnitOfWork Pattern in Project

---
+++ src/MeetingRoomBookingSystem/MeetingRoomBookingSystem.Infrastructure/ApplicationUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿
+using MeetingRoomBookingSystem.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetingRoomBookingSystem.Infrastructure
+{
+    public class ApplicationUnitOfWork : UnitOfWork, IApplicationUnitOfWork
+    {
+        public ApplicationUnitOfWork(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
