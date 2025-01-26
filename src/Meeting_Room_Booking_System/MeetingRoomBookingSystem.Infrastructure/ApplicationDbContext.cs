ae899f25d31be0cffc332922e8fc3de29bb88841
Abdullah Bin Mamun <binbean44@gmail.com>
2025-01-26 12:29:05 +0600

    Seed admin data in User table

--- src/Meeting_Room_Booking_System/MeetingRoomBookingSystem.Infrastructure/ApplicationDbContext.cs
+++ src/Meeting_Room_Booking_System/MeetingRoomBookingSystem.Infrastructure/ApplicationDbContext.cs
@@ -30,5 +30,49 @@ namespace MeetingRoomBookingSystem.Infrastructure
             base.OnConfiguring(optionsBuilder);
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>()
+                .HasData
+                (
+                    new ApplicationUser
+                    {
+                        Id = Guid.Parse("73EFA293-2C32-4746-9ECC-914AE3233251"),
+                        UserName = "admin",
+                        NormalizedUserName = "ADMIN",
+                        PasswordHash = "AQAAAAIAAYagAAAAEJvlP7iT7x94GkCsdqJRk0qGdGTywCXDjEP57/J0lodU+Z2mFSPoU2Trb20dkgYRQA==",
+                        FirstName = "Admin",
+                        Pin = 12345,
+                        Department = "Administration",
+                        SecurityStamp = "2176714C-1B4A-4EA1-ADAC-B34266A85BF2",
+                        ConcurrencyStamp = "A6D75F7A-EB62-4389-8C2E-BF43AAAFA43D"
+                    }
+                );
+
+            builder.Entity<ApplicationRole>()
+                .HasData
+                (
+                    new ApplicationRole
+                    {
+                        Id = Guid.Parse("99C730F7-08F1-4C26-B721-B67FFAE26B94"),
+                        Name = "Admin",
+                        NormalizedName = "ADMIN",
+                        ConcurrencyStamp = "AE06AECF-4D8D-4172-8B1B-38577DF9D250"
+                    }
+                );
+
+            builder.Entity<ApplicationUserRole>()
+                .HasData
+                (
+                    new ApplicationUserRole
+                    {
+                        RoleId = Guid.Parse("99C730F7-08F1-4C26-B721-B67FFAE26B94"),
+                        UserId = Guid.Parse("73EFA293-2C32-4746-9ECC-914AE3233251")
+                    }
+                );
+
+            base.OnModelCreating(builder);
+        }
+
     }
 }
