5f7fe34af773367bda996b06971b8d7b1cf7e689
Abdullah Bin Mamun <binbean44@gmail.com>
2025-01-27 00:39:28 +0600

    Create TempDataExtension.cs

---
+++ src/MeetingRoomBookingSystem/MeetingRoomBookingSystem.Infrastructure/Entensions/TempDataExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace MeetingRoomBookingSystem.Infrastructure.Entensions
+{
+    public static class TempDataExtension
+    {
+        public static void Put<T>(this ITempDataDictionary tempData, string key, T value) where T : class
+        {
+            tempData[key] = JsonSerializer.Serialize(value);
+        }
+
+        public static T? Get<T>(this ITempDataDictionary tempData, string key) where T : class
+        {
+            object? o;
+            tempData.TryGetValue(key, out o);
+            return o == null ? null : JsonSerializer.Deserialize<T>((string)o);
+        }
+
+        public static T? Peek<T>(this ITempDataDictionary tempData, string key) where T : class
+        {
+            object? o = tempData.Peek(key);
+            return o == null ? null : JsonSerializer.Deserialize<T>((string)o);
+        }
+    }
+}
