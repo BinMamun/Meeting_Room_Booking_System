e3ab971639a037e305398cb53e86853ff9e6cb80
Abdullah Bin Mamun <binbean44@gmail.com>
2025-01-26 23:30:34 +0600

    Create meetingroom and booking repository class

---
+++ src/MeetingRoomBookingSystem/MeetingRoomBookingSystem.Infrastructure/Repositories/BookingRepository.cs
@@ -0,0 +1,11 @@
+﻿
+using MeetingRoomBookingSystem.Domain.Entities;
+using MeetingRoomBookingSystem.Domain.RepositoryContracts;
+
+namespace MeetingRoomBookingSystem.Infrastructure.Repositories
+{
+    public class BookingRepository : Repository<Booking, Guid>, IBookingRepository
+    {
+        public BookingRepository(ApplicationDbContext context) :base(context) { }
+    }
+}
---
+++ src/MeetingRoomBookingSystem/MeetingRoomBookingSystem.Infrastructure/Repositories/MeetingRoomRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using MeetingRoomBookingSystem.Domain.Entities;
+using MeetingRoomBookingSystem.Domain.RepositoryContracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetingRoomBookingSystem.Infrastructure.Repositories
+{
+    public class MeetingRoomRepository : Repository<MeetingRoom, Guid>, IMeetingRoomRepository
+    {
+        public MeetingRoomRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
